6b876cb552ca0f00f805dbedefd7cd7a1acdf0e2
gaelgael5 <gaelgael5@gmail.com>
2017-08-29 19:58:26 +0200

    init front

---
+++ front/src/app/applications/applications.component.ts
@@ -0,0 +1,16 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector:"applications",
+    styleUrls: ['./applications.component.css'],
+    templateUrl: './applications.component.html'
+})
+export class Applications {
+
+    constructor() {
+        
+    }
+
+
+}
---
+++ front/src/app/console/console.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:"console",
+    styleUrls: ['./console.component.css'],
+    templateUrl: './console.component.html'
+})
+export class console {
+
+    constructor() {
+        
+    }
+
+
+}
